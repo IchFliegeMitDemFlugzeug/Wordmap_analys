3ad6fa9ca741b42a6b68326f8b3474805f785ea8
IchFliegeMitDemFlugzeug <motorola5888@gmail.com>
2026-09-13 03:31:49 -0700

    1

--- START.cmd
+++ START.cmd
@@ -1,6 +1,17 @@
 @echo off
 cd /d "%~dp0"
 chcp 65001 >nul
+
+set "NODE_DIR=%~dp0.tools\node22"
+set "PATH=%NODE_DIR%;%PATH%"
+
+if not exist "%NODE_DIR%\node.exe" (
+    echo Node.js 22 not found in .tools\node22
+    pause
+    exit /b 1
+)
+
 if not exist node_modules npm install
+
 node main.mjs
 if errorlevel 1 pause
